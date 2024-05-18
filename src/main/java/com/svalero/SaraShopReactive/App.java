6b6f0c8ba49ed913fa3fc7a8dc994fe0c9d98fce
Richard <125735052+RichardMartAgu@users.noreply.github.com>
2024-05-18 10:55:43 +0200

    Initial commit

---
+++ src/main/java/com/svalero/SaraShopReactive/App.java
@@ -0,0 +1,19 @@
+package com.svalero.SaraShopReactive;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+
+    // Método principal de inicio de la aplicación
+    @Override
+    public void start(Stage stage) throws IOException {
+    }
+
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
